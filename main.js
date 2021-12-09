f8254aa8faaca8104ab54745276aef503a1e91de
Numbone <77365959+Numbone@users.noreply.github.com>
2021-12-09 17:56:21 +0600

    das
    das

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$(function(){
+
+    $('.slick-slider').slick({
+        slidesToShow:3,
+        slidestoScroll:2,
+        autoplay:true,
+        autoplaySpeed:0500,
+        arrow:true
+    })
+})
+
+
---
+++ slick.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.slick-slider2').slick({
+        slidesToShow:1,
+        slidestoScroll:1,
+        autoplay:true,
+        autoplaySpeed:3000,
+        arrow:true
+    })
+})
